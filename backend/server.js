147dcb866a562a417e3ad1b6263a5e0df8cdc01d
Rutuja Kagwade <rutujakagwade5@example.com>
2025-05-29 16:50:51 +0530

    Fix: updated

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,30 @@
-require('dotenv').config();
-console.log('MONGO_URI:', process.env.MONGO_URI); // Debug line
-
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const taskRoutes = require('./routes/taskRoutes');
 const errorHandler = require('./middleware/errorMiddleware');
 
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Connect DB
 connectDB();
 
-// Middleware
-app.use(cors());
+// CORS Middleware
+app.use(cors({
+  origin: ['https://todolist-liart-iota-60.vercel.app'],
+  credentials: true
+}));
+app.options('*', cors());
+
+// Body Parser
 app.use(express.json());
 
 // Routes
 app.use('/api/tasks', taskRoutes);
 
-// Error Middleware
+// Error Handler
 app.use(errorHandler);
 
 // Start server
